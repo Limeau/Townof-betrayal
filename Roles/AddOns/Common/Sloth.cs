ae28b419fcd76c00ad7d450f5b985a05a56a0cdb
Dailyhare <156022382+Dailyhare@users.noreply.github.com>
2024-01-11 17:54:15 -0700

    Create Sloth.cs

---
+++ Roles/AddOns/Common/Sloth.cs
@@ -0,0 +1,26 @@
+﻿using AmongUs.GameOptions;
+using static TOHE.Options;
+
+namespace TOHE.Roles.AddOns.Common;
+
+public static class Sloth
+{
+    private static readonly int Id = 27893;
+
+    private static OptionItem OptionSpeed;
+
+    public static void SetupCustomOption()
+    {
+        SetupAdtRoleOptions(Id, CustomRoles.Sloth, canSetNum: true, tab: TabGroup.Addons);
+        OptionSpeed = FloatOptionItem.Create(Id + 10, "SlothSpeed", new(0.25f, 5f, 0.25f), 0.5f, TabGroup.Addons, false)
+            .SetParent(CustomRoleSpawnChances[CustomRoles.Sloth])
+            .SetValueFormat(OptionFormat.Multiplier);
+    }
+    public static void SetSpeed(byte playerId, bool clearAddOn)
+    {
+        if (!clearAddOn)
+            Main.AllPlayerSpeed[playerId] = OptionSpeed.GetFloat();
+        else
+            Main.AllPlayerSpeed[playerId] = Main.RealOptionsData.GetFloat(FloatOptionNames.PlayerSpeedMod);
+    }
+}
